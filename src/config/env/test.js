8e81f7a02cd59fea075e9c07587ae0ef4326eb0b
Kevin Depuydt <kevin@botfuel.io>
2017-10-17 15:18:20 +0200

    fix tests

--- src/config/env/test.js
+++ src/config/env/test.js
@@ -9,9 +9,26 @@ const productionEnv = {
   },
   nodeEnv: 'test',
   socialCredentials: {
-    facebook: {},
-    google: {},
-    linkedin: {},
+    facebook: {
+      clientID: process.env.FACEBOOK_ID || 'facebook_id',
+      clientSecret: process.env.FACEBOOK_SECRET || 'facebook_secret',
+    },
+    google: {
+      clientID: process.env.GOOGLE_ID || 'google_id',
+      clientSecret: process.env.GOOGLE_SECRET || 'google_secret',
+    },
+    linkedin: { // credentials to replace
+      clientID: process.env.LINKEDIN_ID || 'linkedin_id',
+      clientSecret: process.env.LINKEDIN_SECRET || 'linkedin_secret',
+    },
+    github: {
+      clientID: process.env.GITHUB_ID || 'github_id',
+      clientSecret: process.env.GITHUB_SECRET || 'github_secret',
+    },
+    twitter: {
+      clientID: process.env.TWITTER_ID || 'twitter_id',
+      clientSecret: process.env.TWITTER_SECRET || 'twitter_secret',
+    },
   },
 };
 
