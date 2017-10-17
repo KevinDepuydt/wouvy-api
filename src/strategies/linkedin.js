8a4564ca5bf6e03b50669d96743feac964dd9315
Kevin Depuydt <kevin@botfuel.io>
2017-10-17 17:35:10 +0200

    update linkedin strategy

--- src/strategies/linkedin.js
+++ src/strategies/linkedin.js
@@ -1,4 +1,5 @@
 import LinkedInStrategy from 'passport-linkedin';
+import User from '../models/user';
 
 const linkedinStrategy = (passport, linkedinConfig) => {
   passport.use(new LinkedInStrategy({
@@ -8,13 +9,33 @@ const linkedinStrategy = (passport, linkedinConfig) => {
     profileFields: ['id', 'first-name', 'last-name', 'email-address', 'picture-url'],
     passReqToCallback: true,
   }, (req, accessToken, refreshToken, profile, done) => {
-    // @TODO look for user with email + providerName.id eql to profile.id
-    console.log('*** LINKEDIN AUTH CALLBACK ***');
-    console.log(accessToken);
-    console.log(refreshToken);
-    console.log(profile);
-    console.log('*** END ***');
-    done();
+    const linkedinData = profile._json;
+    // find user by mail and linkedin provider id
+    User.findOne({ email: linkedinData.emailAddress, 'providers.linkedin.id': linkedinData.id }, (err, user) => {
+      if (err) {
+        done(err, null);
+      } else if (user) {
+        done(null, user);
+      } else {
+        // define new User
+        const newUser = new User({
+          email: linkedinData.emailAddress,
+          firstname: linkedinData.firstName,
+          lastname: linkedinData.lastName,
+          password: Math.random().toString(36).slice(2),
+          picture: linkedinData.pictureUrl,
+          'providers.linkedin': {
+            id: linkedinData.id,
+            accessToken,
+            refreshToken,
+          },
+        });
+        // add new User
+        newUser.save()
+          .then(savedUser => done(null, savedUser))
+          .catch(errUser => done(errUser, null));
+      }
+    });
   }));
 };
 
