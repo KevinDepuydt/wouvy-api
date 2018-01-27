6debd0ce1202b871929e16e9d01bcabecbf4a173
KevinDepuydt <kevin.dpdt@gmail.com>
2018-01-27 17:54:06 +0100

    fixes

---
+++ src/controllers/members.js
@@ -0,0 +1,93 @@
+import mongoose from 'mongoose';
+import _ from 'lodash';
+import Member from '../models/member';
+import env from '../config/env';
+
+const rights = env.rights;
+
+/**
+ * Create a Member
+ */
+const create = (req, res) => {
+  const member = new Member(req.body);
+
+  member.save()
+    .then(savedDoc => res.jsonp(savedDoc))
+    .catch(err => res.status(500).send({ message: err }));
+};
+
+/**
+ * Show the current Member
+ */
+const read = (req, res) => {
+  const member = req.member ? req.member.toJSON() : {};
+
+  // is member moderator of the workflows
+  member.isModerator = member.rights.workflows === rights.MODERATE.value;
+
+  res.jsonp(member);
+};
+
+/**
+ * Update a Member
+ */
+const update = (req, res) => {
+  let member = req.member;
+
+  const memberRights = _.assign({}, member.rights);
+
+  member = _.extend(member, req.body);
+
+  if (req.body.rights) {
+    member.rights = _.extend(memberRights, req.body.rights);
+  }
+
+  member.save()
+    .then(savedDoc => res.jsonp(savedDoc))
+    .catch(err => res.status(500).send({ message: err }));
+};
+
+/**
+ * Remove an Member
+ */
+const remove = (req, res) => {
+  const member = req.member;
+
+  member.remove()
+    .then(removedMember => res.jsonp(removedMember))
+    .catch(err => res.status(500).send({ message: err }));
+};
+
+/**
+ * List of Member
+ */
+const list = (req, res) => {
+  Member.find().sort('-created').exec()
+    .then(members => res.jsonp(members))
+    .catch(err => res.status(500).send({ message: err }));
+};
+
+/**
+ * Member middleware
+ */
+const memberByID = (req, res, next, id) => {
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send({
+      message: 'Member id is not valid',
+    });
+  }
+
+  Member.findById(id)
+    .then((member) => {
+      if (!member) {
+        return res.status(404).send({
+          message: 'Member not found',
+        });
+      }
+      req.member = member;
+      next();
+    })
+    .catch(err => next(err));
+};
+
+export { create, read, update, remove, list, memberByID };
---
+++ test/members.js
@@ -0,0 +1,124 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import env from '../src/config/env';
+import User from '../src/models/user';
+import Member from '../src/models/member';
+import Workflow from '../src/models/workflow';
+
+// add chaiHttp to chai
+chai.use(chaiHttp);
+
+// define base url
+const BASE_API_URL = `http://${env.host}:${env.port}`;
+
+// call chai should
+chai.should();
+
+const USER_DATA = { email: 'test@wouvy.fr', firstname: 'First', lastname: 'Last', password: 'Password' };
+
+const membersTests = () => {
+  afterEach((done) => {
+    User.remove({}, () => {
+      Member.remove({}, () => done());
+    });
+  });
+
+  describe('List', () => {
+    it('it should GET all the members', (done) => {
+      chai.request(BASE_API_URL)
+        .get('/api/members')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('array');
+          res.body.length.should.be.eql(0);
+          done();
+        });
+    });
+  });
+
+  describe('Create', () => {
+    it('it should POST a member', (done) => {
+      const user = new User(USER_DATA);
+      const workflow = new Workflow({ name: 'test' });
+      user.save((error, savedUser) => {
+        const memberData = { user: savedUser, workflowId: workflow._id };
+        chai.request(BASE_API_URL)
+          .post('/api/members')
+          .send(memberData)
+          .end((err, res) => {
+            console.error(err);
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            res.body.user.should.be.eql(savedUser._id.toString());
+            done();
+          });
+      });
+    });
+  });
+
+  describe('Read', () => {
+    it('it should GET a member by id', (done) => {
+      const user = new User(USER_DATA);
+      const workflow = new Workflow({ name: 'test' });
+      user.save((errorUser, savedUser) => {
+        const member = new Member({ user: savedUser, workflowId: workflow._id });
+        member.save((errorMember, savedMember) => {
+          chai.request(BASE_API_URL)
+            .get(`/api/members/${savedMember._id}`)
+            .end((err, res) => {
+              res.should.have.status(200);
+              res.body.should.be.a('object');
+              res.body._id.should.be.eql(savedMember._id.toString());
+              res.body.user.should.be.eql(savedUser._id.toString());
+              done();
+            });
+        });
+      });
+    });
+  });
+
+  describe('Update', () => {
+    it('it should PUT a member', (done) => {
+      const updates = { rights: { workflow: env.rights.PARTICIPATE.level } };
+      const user = new User(USER_DATA);
+      const workflow = new Workflow({ name: 'test' });
+      user.save((errorUser, savedUser) => {
+        const member = new Member({ user: savedUser, workflowId: workflow._id });
+        member.save((errorMember, savedMember) => {
+          chai.request(BASE_API_URL)
+            .put(`/api/members/${savedMember._id}`)
+            .send(updates)
+            .end((err, res) => {
+              res.should.have.status(200);
+              res.body.should.be.a('object');
+              res.body.rights.workflow.should.be.eql(env.rights.PARTICIPATE.level);
+              res.body.rights.member.should.be.eql(env.rights.NONE.level);
+              done();
+            });
+        });
+      });
+    });
+  });
+
+  describe('Remove', () => {
+    it('it should DELETE a member', (done) => {
+      const user = new User(USER_DATA);
+      const workflow = new Workflow({ name: 'test' });
+      user.save((errorUser, savedUser) => {
+        const member = new Member({ user: savedUser, workflowId: workflow._id });
+        member.save((errorMember, savedMember) => {
+          chai.request(BASE_API_URL)
+            .delete(`/api/members/${savedMember._id}`)
+            .end((err, res) => {
+              res.should.have.status(200);
+              res.body.should.be.a('object');
+              res.body.user.should.be.eql(savedUser._id.toString());
+              done();
+            });
+        });
+      });
+    });
+  });
+};
+
+export default membersTests;
